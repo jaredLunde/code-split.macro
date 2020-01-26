37209e8a9fca0b3f282a0bf56c11b2ea3c5c99c9
Jared Lunde <jared.lunde@gmail.com>
2020-01-26 13:53:45 -0700

    Fixes those so-called improved types

--- types/macro.d.ts
+++ types/macro.d.ts
@@ -1,9 +1,12 @@
 declare module 'code-split.macro' {
-  function codeSplit<T = any>(path: string, isServerBuild: false): Promise<T>
-  function codeSplit<T = any>(path: string, isServerBuild: true): T
+  function codeSplit<T = any>(
+    path: string,
+    isServerBuild: false
+  ): () => Promise<T>
+  function codeSplit<T = any>(path: string, isServerBuild: true): () => T
   function codeSplit<T = any>(
     path: string,
     isServerBuild: boolean
-  ): Promise<T> | T
+  ): () => Promise<T> | T
   export default codeSplit
 }
